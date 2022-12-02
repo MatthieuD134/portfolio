e368dd06e101491055215def90c182dddf5b471b
Matthieu Daulhiac <matthieu@blanknetwork.com>
2022-12-02 20:46:15 +0100

    finalise theme switcher

--- src/components/common/ThemeSelector/index.tsx
+++ src/components/common/ThemeSelector/index.tsx
@@ -1,5 +1,9 @@
 import { THEMES } from '@src/common/constants';
 import { useEffect, useState } from 'react';
+import styles from './index.module.scss';
+
+import LightModeLogo from '@src/common/icons/lightMode.svg';
+import DarkModeLogo from '@src/common/icons/darkMode.svg';
 
 const ThemeSelector = () => {
   const [theme, setTheme] = useState(THEMES.LIGHT);
@@ -19,8 +23,26 @@ const ThemeSelector = () => {
           ? handleChangeTheme(THEMES.DARK)
           : handleChangeTheme(THEMES.LIGHT)
       }
+      className={styles.themeSwitcher}
     >
-      change theme
+      <div className={styles.background}>
+        <span
+          className={`${styles.iconSelector} ${
+            theme === THEMES.LIGHT ? styles.left : styles.right
+          }`}
+        />
+        <div className={styles.iconContainer}>
+          <LightModeLogo
+            className={`${styles.icon} ${
+              theme === THEMES.DARK ? styles.lightIcon : styles.darkIcon
+            }`}
+          />
+        </div>
+
+        <div className={styles.iconContainer}>
+          <DarkModeLogo className={`${styles.icon} ${styles.darkIcon}`} />
+        </div>
+      </div>
     </button>
   );
 };
